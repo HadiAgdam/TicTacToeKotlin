0d2bc4b306aa30315cee63f5d89d8539ebf126fd
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-08-23 21:57:18 +0330

    updated tests using loop

--- src/test/kotlin/BoardWinTest.kt
+++ src/test/kotlin/BoardWinTest.kt
@@ -5,36 +5,48 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class BoardWinTest {
-    
+
     @Test
     fun horizontalTest() {
         val b = Board(3)
-        
-        b.set(Position(0, 0), Mark.X)
-        b.set(Position(1, 0), Mark.X)
-        b.set(Position(2, 0), Mark.X)
 
-        Assertions.assertEquals(Mark.X, b.checkWin())
+        for (i in 0 until 3) {
 
-        b.set(Position(2, 0), Mark.BLANK)
-        Assertions.assertNull(b.checkWin())
+            b.set(Position(0, i), Mark.X)
+            Assertions.assertEquals(null, b.checkWin())
+
+            b.set(Position(1, i), Mark.X)
+            Assertions.assertEquals(null, b.checkWin())
+
+
+            b.set(Position(2, i), Mark.X)
+            Assertions.assertEquals(Mark.X, b.checkWin())
+            b.clear()
+        }
     }
-    
-    
+
+
     @Test
     fun verticalTest() {
         val b = Board(3)
 
-        b.set(Position(0, 0), Mark.X)
-        b.set(Position(0, 1), Mark.X)
-        b.set(Position(0, 2), Mark.X)
+        for (i in 0 until 3) {
+            b.set(Position(i, 0), Mark.X)
+            Assertions.assertEquals(null, b.checkWin())
 
-        Assertions.assertEquals(Mark.X, b.checkWin())
+            b.set(Position(i, 1), Mark.X)
+            Assertions.assertEquals(null, b.checkWin())
+
+
+            b.set(Position(i, 2), Mark.X)
+            Assertions.assertEquals(Mark.X, b.checkWin())
+            b.clear()
+        }
     }
-    
-    
+
+
     @Test
-    fun diagonalTest1(){
+    fun diagonalTest1() {
         val b = Board(3)
 
         b.set(Position(2, 0), Mark.X)
