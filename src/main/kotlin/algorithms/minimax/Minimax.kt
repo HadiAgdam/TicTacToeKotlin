7c05a826a3e94a2bd3ce4aa06d01d54a6672dda0
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-08-26 00:16:26 +0330

    created simple minimax function

--- src/main/kotlin/algorithms/minimax/Minimax.kt
+++ src/main/kotlin/algorithms/minimax/Minimax.kt
@@ -3,31 +3,59 @@ package algorithms.minimax
 import board.Board
 import board.Mark
 import board.Position
+import kotlin.math.abs
 
+object Minimax {
+    private fun minimax(board: Board, mark: Mark, depth: Int = 10): Int {
+        if (depth == 0) return 0
 
-fun minimax(board: Board, c: Mark, depth: Int = 10): Int {
-    return 0
-}
-
-
-fun play(board: Board, c: Mark): Position {
-
-    val b = board.copy()
-    val blanks = b.getBlanks()
-
-    var highestVal = Int.MIN_VALUE
-    var highestPosition: Position? = null
-
-    for (blank in blanks) {
-        board.set(blank, c)
-
-        val m = minimax(b, c)
-        if (m > highestVal) {
-            highestPosition = blank
-            highestVal = m
+        when (board.checkWin()) {
+            mark -> return depth
+            mark.opponent() -> return -depth
+            else -> {}
         }
+
+        var highest = 0
+
+        for (blank in board.getBlanks()) {
+            board.set(blank, mark.opponent())
+            val l = -minimax(board, mark.opponent(), depth - 1)
+
+
+            if (abs(l) > highest) highest = l
+
+            board.set(blank, Mark.BLANK)
+        }
+
+        return highest
     }
 
 
-    return highestPosition!!
+    fun play(board: Board, c: Mark): Position {
+
+        val blanks = board.getBlanks()
+
+        var highestVal = Int.MIN_VALUE
+        var highestPosition: Position? = null
+
+
+        for (blank in blanks) {
+            board.set(blank, c)
+
+            val m = minimax(board, c)
+
+            println("${blank.x} ${blank.y} : $m")
+
+            if (m > highestVal) {
+                highestPosition = blank
+                highestVal = m
+            }
+            board.set(blank, Mark.BLANK)
+        }
+
+
+
+
+        return highestPosition!!
+    }
 }
